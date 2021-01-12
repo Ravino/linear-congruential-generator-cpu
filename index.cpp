8018fdc9a90fcb923660b271acc3c68fb68089f8
Ravino Doul <evgeniy@nekrasov.pw>
2021-01-12 13:41:35 +1000

    Base variant using randomizer

--- index.cpp
+++ index.cpp
@@ -1,5 +1,20 @@
+#include <vector>
 #include "randomizer.h"
 
+
+
+
 int main () {
+
+  unsigned length = 1000;
+
+
+  std::vector<unsigned> *vec = new std::vector<unsigned>[length];
+  vec -> reserve (length);
+
+
+  double result = randomizer(vec, length, 10, 100);
+
+
   return 0;
 }
